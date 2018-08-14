b26999716dfcece9ba3794cab621fc110d52f1c8
GesukaLeo <36921762+GesukaLeo@users.noreply.github.com>
2018-08-14 12:56:18 +0300

    Updated all the imports to conform to the modules

--- test_circles.py
+++ test_circles.py
@@ -1,10 +1,11 @@
 import unittest
+from circles import circle_area
 from math import pi
-from circle_Area import *
 
 class TestCircleArea(unittest.TestCase):
-    def test_circle_Area(self):
+    def test_circles_Area(self):
         #test radius when radius >= 0
         self.assertAlmostEqual(circle_area(1), pi)
         self.assertAlmostEqual(circle_area(0), 0)
+        self.assertAlmostEqual(circle_area(2.1), pi * 2.1 ** 2)
 
